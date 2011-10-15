4d3013fec66263d37430f3b4d2c9e543e3d216d8
ezterry <ezterry3@gmail.com>
2011-10-15 12:55:26 -0400

    Switch to a prebuilt superuser v3.0.5 (39)
    Super user now has Honeycomb code; and is using an API to provide
    backwards compatibility.  However to build this compatibility layer
    we need the Honeycomb SDK.
    
    AS such I have used a Honeycomb compatible apktool to add:
    android:sharedUserId="android.uid.superuser"
    to the android manifest;
    
    This will allow the block superuser updates feature to operate.
    
    Otherwise this is the same binary as provided by ChainsDD

--- Android.mk
+++ Android.mk
@@ -1,12 +1,4 @@
+#Switch to a local prebuilt version of superuser
+
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_PACKAGE_NAME := Superuser
-LOCAL_SRC_FILES := $(call all-java-files-under,src)
-
-LOCAL_CERTIFICATE := superuser
-
-include $(BUILD_PACKAGE)
-
-##
-
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/Superuser.apk:system/app/Superuser.apk
